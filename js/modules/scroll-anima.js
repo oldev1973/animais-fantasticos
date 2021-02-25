68e903511efc89d1bfc7d8c021cbbde8301cad9d
trindade-fernando <trindade-fernando@outlook.com>
2021-02-25 18:49:19 -0300

    refatoring scrollAnima with class

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,26 @@
+// scroll quando a section começa a aparecer na tela
+
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowHalf = window.innerHeight * 0.5;
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowHalf < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener("scroll", this.animaScroll);
+  }
+}
