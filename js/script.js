9f032add5836b3eafacd9edd6c5086e8d9115621
trindade-fernando <trindade-fernando@outlook.com>
2021-02-28 11:22:30 -0300

    animaisapi path ok

--- js/script.js
+++ js/script.js
@@ -48,5 +48,5 @@ menuMobile.init();
 const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 fetchBitcoins("https://blockchain.info/ticker", ".btc-preco");
